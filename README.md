17ad5d7ddd6164a9ab3afbfb2ef64ac3ac55beb1
Yunchi Luo <mightyguava@gmail.com>
2020-02-29 17:44:38 -0500

    add ::text and ::attr pseudoselectors

--- README.md
+++ README.md
@@ -8,8 +8,15 @@ HTML query CLI using CSS selectors, built on top of [Cascadia](https://github.co
 go get github.com/mightyguava/hq
 ```
 
-## usage
+## Usage
 
 ```
 hq 'div.class_name' test.html
+cat test.html | hq 'div.class_name'
 ```
+
+## Special pseudo-selectors
+
+Use the `::text` pseudo-selector to recursively print the text nodes of matching nodes instead of rendering their HTML. Its behavior is the same as setting `--text`.
+
+Use the `::attr(<attr>)` pseuo-selector to print the value of a matching attribute in the matching nodes instead of than rendering the their html. For example, `::attr(href)` prints the `href` value of the matching nodes. Each match is printed on a separate line. Non-matching nodes are skipped.
--- main.go
+++ main.go
@@ -3,13 +3,20 @@ package main
 import (
 	"bytes"
 	"fmt"
+	"io"
 	"os"
+	"regexp"
+	"strings"
 
 	"github.com/alecthomas/kong"
 	"github.com/andybalholm/cascadia"
 	"github.com/yosssi/gohtml"
 	"golang.org/x/net/html"
 )
+
+const pseudoSelectorText = "::text"
+
+var pseudoSelectorAttr = regexp.MustCompile(`::attr\(([^)"'>/=\s]+)\)\s*$`)
 
 // Runs a query selector query using goquery on the given file
 func main() {
@@ -28,16 +35,33 @@ func run() error {
 	}{}
 	kong.Parse(&cli)
 
-	f, err := os.Open(cli.In)
+	var in io.Reader
+	if cli.In == "" {
+		f, err := os.Open(cli.In)
+		if err != nil {
+			return err
+		}
+		defer f.Close()
+		in = f
+	} else {
+		in = os.Stdin
+	}
+	doc, err := html.Parse(in)
 	if err != nil {
 		return err
 	}
-	defer f.Close()
-	doc, err := html.Parse(f)
-	if err != nil {
-		return err
+	rawSelector := cli.Selector
+	if strings.HasSuffix(rawSelector, pseudoSelectorText) {
+		rawSelector = strings.TrimSuffix(rawSelector, pseudoSelectorText)
+		cli.Text = true
 	}
-	selector, err := cascadia.Compile(cli.Selector)
+	selectAttr := ""
+	if matches := pseudoSelectorAttr.FindStringSubmatch(rawSelector); matches != nil {
+		selectAttr = strings.ToLower(matches[1])
+		idx := pseudoSelectorAttr.FindStringIndex(rawSelector)[0]
+		rawSelector = rawSelector[0:idx]
+	}
+	selector, err := cascadia.Compile(rawSelector)
 	if err != nil {
 		return err
 	}
@@ -61,7 +85,14 @@ func run() error {
 		}
 	} else {
 		for _, n := range nodes {
-			if err := html.Render(buf, n); err != nil {
+			if selectAttr != "" {
+				for _, attr := range n.Attr {
+					if selectAttr == strings.ToLower(attr.Key) {
+						buf.WriteString(attr.Val)
+						buf.WriteRune('\n')
+					}
+				}
+			} else if err := html.Render(buf, n); err != nil {
 				return err
 			}
 		}
